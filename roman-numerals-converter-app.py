8c70b1cb2c3d0810db79f7e687339405f7ced18a
ihsanerdogan <ihsanerdogan@gmail.com>
2023-09-17 13:28:58 +0200

    ihsan

---
+++ roman-numerals-converter-app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request
+from collections import OrderedDict
+
+app = Flask(__name__)
+
+def converter(num):
+
+    roman = OrderedDict()
+    roman[1000] = "M"
+    roman[900] = "CM"
+    roman[500] = "D"
+    roman[400] = "CD"
+    roman[100] = "C"
+    roman[90] = "XC"
+    roman[50] = "L"
+    roman[40] = "XL"
+    roman[10] = "X"
+    roman[9] = "IX"
+    roman[5] = "V"
+    roman[4] = "IV"
+    roman[1] = "I"
+
+    def roman_num(num):
+        for r in roman.keys():
+            x, y = divmod(num, r)
+            yield roman[r] * x
+            num -= (r * x)
+            if num <= 0:
+                break
+
+    return "".join([a for a in roman_num(num)])
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", methods=["GET"])
+
+@app.route("/convert", methods=["GET", "POST"])
+def convert():
+    if request.method == "POST":
+        num = request.form.get("number")
+        return render_template("result.html", number_decimal = num, number_roman = converter(int(num)), developer_name = 'ihsan')
+    else:
+        return render_template("result.html", developer_name = "ihsan")
+
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=80)
